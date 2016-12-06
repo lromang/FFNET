61e8f3031f821601808c129c941bab38a51034f5
lromang <luis.roangarci@gmail.com>
2016-12-06 16:00:49 -0600

    multiple layer

--- code/oracularTuring.java
+++ code/oracularTuring.java
@@ -25,6 +25,24 @@ import Jama.*;
 
 public class oracularTuring{
 
+    // Public variables
+    public static int   nHLayers    = 4;
+    public static int[] widthLayer = {3, 5, 7, 4, 1}; // Includes input layer
+
+    /*
+     * ---------------------------------------------
+     * Generate initial weights
+     * ---------------------------------------------
+     * x = observation
+     */
+    private static Matrix[] initW(){
+        Matrix[] layerWeights = new Matrix[nHLayers];
+        for(int i = 0; i < nHLayers; i++){
+            layerWeights[i] = Matrix.random(widthLayer[i + 1], widthLayer[i] + 1);
+        }
+        return layerWeights;
+    }
+
     /*
      * ---------------------------------------------
      * Add bias
@@ -43,12 +61,12 @@ public class oracularTuring{
 
     /*
      * ---------------------------------------------
-     * Activation layer
+     * Hidden layer with activation option
      * ---------------------------------------------
      * w = weight matrix
      * x = observation
      */
-    private static Matrix actLayer(double[][] w, double[] x){
+    private static double[] hiddLayer(double[][] w, double[] x, char activation){
         Matrix W        = new Matrix(w);
         Matrix b        = new Matrix(addBias(x), 1).transpose();
         Matrix H        = W.times(b);
@@ -57,20 +75,69 @@ public class oracularTuring{
         double[] hidden = new double[n_hidden];
         // Apply tanh
         for(int i = 0; i < n_hidden; i++){
-            hidden[i] = Math.tanh(H.get(i, 0));
+            if(activation == 'l'){
+                hidden[i] = Math.log(H.get(i, 0));
+            }else{
+                hidden[i] = Math.tanh(H.get(i, 0));
+            }
         }
-        return H;
+        return hidden;
     }
 
-    public static void main(String args[]){
-        Matrix A = Matrix.random(5, 3);
-        Matrix b = Matrix.random(1, 3);
-        // Matrix AB      = AM.times(bM);
-        Matrix H = actLayer(A.getArray(), b.getArray()[][0]);
-        System.out.println("Dimension columnas b");
-        System.out.println(H.getColumnDimension());
-        System.out.println("Dimension renglones b");
-        System.out.println(H.getRowDimension());
+    /*
+     * ---------------------------------------------
+     * Hidden layer with default logistic
+     * ---------------------------------------------
+     * w = weight matrix
+     * x = observation
+     */
+    private static double[] hiddLayer(double[][] w, double[] x){
+        return hiddLayer(w, x, 'l');
+    }
 
+
+    /*
+     * ---------------------------------------------
+     * Output layer with activation option
+     * ---------------------------------------------
+     * w = weight matrix
+     * x = observation
+     */
+    private static double outputLayer(double[][] w, double[] x, char activation){
+        Matrix W    = new Matrix(w);
+        Matrix b    = new Matrix(addBias(x), 1).transpose();
+        double pred = W.times(b).get(1, 1);
+        if(activation == 'l'){
+            return Math.log(pred);
+        }else{
+            return Math.tanh(pred);
+        }
+    }
+
+    /*
+     * ---------------------------------------------
+     * Output layer with default logistic
+     * ---------------------------------------------
+     * w = weight matrix
+     * x = observation
+     */
+    private static double outputLayer(double[][] w, double[] x){
+        return outputLayer(w, x, 'l');
+    }
+
+    /*
+     * ---------------------------------------------
+     * MAIN
+     * ---------------------------------------------
+     */
+    public static void main(String args[]){
+        Matrix   W1 = Matrix.random(5, 4);
+        Matrix   W2 = Matrix.random(5, 4);
+        double[] x  = {.1, .1, .1};
+        double[] y  = hiddLayer(W1.getArray(), x);
+        Matrix[] layers = initW();
+        for(int i = 0; i < layers.length; i++){
+            System.out.println("Número de filas: " + layers[i].getRowDimension() + " | Número de columnas: " + layers[i].getColumnDimension());
+        }
     }
 }
